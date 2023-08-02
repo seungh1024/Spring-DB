6c1b73d7af1a9ba44dcf00a82e076d2b7011142a
seungh <seungh1024@naver.com>
2023-08-02 23:31:50 +0900

    Spring DB 트랜잭션 문제 해결 학습
    순수 자바 코드로 트랜잭션 적용 시 발생하는 구현 기술이 서비스 계층에 노출되는 문제, 트랜잭션 동기화 때문에 커넥션을 파라미터로 넘겨주는 문제, 반복적인 트랜잭션 코드 문제 해결

---
+++ server/spring-db-basic/src/main/java/com/seungh1024/service/MemberServiceV3_1.java
@@ -0,0 +1,58 @@
+package com.seungh1024.service;
+
+import com.seungh1024.domain.Member;
+import com.seungh1024.repository.MemberRepositoryV2;
+import com.seungh1024.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+//    private final DataSource dataSource;
+    private final PlatformTransactionManager transactionManager;
+    public final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        //트랜잭션 시작
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try{
+            //비즈니스 로직 수행
+            bizLogic(fromId, toId, money);
+            transactionManager.commit(status);
+
+        }catch(Exception e){
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId,fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId,toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+}
